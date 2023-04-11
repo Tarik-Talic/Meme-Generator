ab1a2508e69d009a7ccec1f5629cd53ed80c73f3
Tarik-Talic <talic.tarik@gmail.com>
2023-04-11 23:44:09 +0200

    local data file

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./styles/Meme.css";
+import memesData from "../memeData";
 
 export default function Meme() {
+  function getMemeImg() {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[randomNumber].url;
+    console.log(url);
+  }
   return (
-    <form>
+    <div className="form">
       <div className="form-inputs">
-        <input />
-        <input />
+        <input placeholder="Top Text" />
+        <input placeholder="Bottom Text" />
       </div>
-      <button>Get a new meme image </button>
-    </form>
+      <button onClick={getMemeImg}>Get a new meme image </button>
+    </div>
   );
 }
