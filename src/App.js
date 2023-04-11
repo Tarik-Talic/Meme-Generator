c43d46acd299e700701111e390621a31cb9ee99b
Tarik-Talic <talic.tarik@gmail.com>
2023-04-11 23:24:07 +0200

    inputs and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Meme from "./components/Meme";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme className="meme-form" />
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/Meme.css";
+
+export default function Meme() {
+  return (
+    <form>
+      <div className="form-inputs">
+        <input />
+        <input />
+      </div>
+      <button>Get a new meme image </button>
+    </form>
+  );
+}
